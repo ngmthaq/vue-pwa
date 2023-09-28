5abcaddf78ca18f13ee501bafa5b60951edba2be
Nguyen Manh Thang <nguyenmanhthang2000.fb@gmail.com>
2023-09-28 13:54:36 +0700

    Update dev version

--- version.js
+++ version.js
@@ -3,15 +3,15 @@
 try {
   const mode = process.argv[2];
   const fs = require("fs");
+  const pkg = require("./package.json");
 
   let version = "";
 
   if (!mode) throw new Error("ERROR: Cannot find mode");
 
   if (mode === "dev") {
-    version = Date.now() + "-dev";
+    version = pkg.version + "." + Date.now() + "-dev";
   } else if (mode === "prod") {
-    const pkg = require("./package.json");
     version = pkg.version;
   } else {
     throw new Error("ERROR: Cannot find mode");
